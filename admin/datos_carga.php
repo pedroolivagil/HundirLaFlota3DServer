6634928b9073533c4db4788d634de3bde56c1cb4
pedroolivagil <onion_oliva@hotmail.com>
2017-06-24 21:16:09 +0200

    Persistence: User

--- admin/datos_carga.php
+++ admin/datos_carga.php
@@ -1,7 +1,23 @@
 <?php
 
 include_once('ScriptDB.php');
-//header('Content-type: application/json; charset=utf-8');
+header('Content-type: text/plain; charset=utf-8');
 
 $script = new ScriptDB();
-$script->loadDefaultData();
+// Iniciamos los controladores
+$userManager = new UserController();
+
+// Datos
+$fileUsers = file("users.db", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+// Insertamos los datos a la BBDD
+echo "\nUsuarios";
+foreach ($fileUsers as $num_línea => $linea) {
+    $user = new User(json_decode($linea, TRUE));
+    if ($userManager->create($user)) {
+        echo "\n\tInsertado\t" . $user->getUsername();
+    } else {
+        echo "\n\tNO insertado\t" . $user->getUsername();
+    }
+}
+echo "\n--------------------------------------------------------------------";
