82b4e2e855c301b6695c3ad9fd9c774d13bb21e8
pedroolivagil <onion_oliva@hotmail.com>
2017-08-02 19:40:03 +0200

    Persistence: Modelos y Controladores (no furula bien)

--- userlog.php
+++ userlog.php
@@ -17,6 +17,37 @@ include_once('config.php');
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <style>
+            tr {
+                width: 100%;
+                display: inline-table;
+                table-layout: fixed;
+            }
+
+            table{
+                height:700px;              /*Select the height of the table*/
+                display: -moz-groupbox;    /* Firefox Bad Effect*/
+            }
+            thead{
+                width: 98% !important;
+                position: absolute;
+            }
+            tbody{
+                overflow-y: scroll;      
+                height: 710px;            /* Select the height of the body*/
+                width: 99.5%;
+                position: absolute;
+                top:38px;
+            } 
+            td {
+                overflow: hidden;
+                min-height: 20px;
+            }
+            td:first-child {
+                width: 50px !important;
+                text-align: center;
+            }
+        </style>
     </head>
     <body>
         <table class="table table-bordered table-responsive table-striped">
